cab72a2635089088024f5ee50bf83e076bd3eb50
Daniel Samek <samek@spstrutnov.cz>
2023-04-08 19:02:14 +0200

    command

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,12 @@
 std::string FEN = "rnbqkbnr/8/8/pppppppp/PPPP1PPP/8/8/RNBQKBNR w KQkq h6 0 1";
 
 int main(int argc, char* argv[]){
-	if(argv[1]) FEN = argv[1];
-	Board::parseFen(FEN);
+	if(argv[1] == "FEN"){
+		Board::parseFen(FEN);
+	} 
+	if (argv[1] == "perft"){
+		int perftDepth = (int)argv[2] - '0';
+		// Todo
+	} 
 	return 0;
 }
